557b895de3cbd3ba15bcbbbcb63e57ad301da1fa
LinqManJr <johnybond32@yandex.ru>
2019-10-18 21:26:17 +0900

    add loginmodel

---
+++ Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookieAuthentication.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Input Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Incorrect password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
